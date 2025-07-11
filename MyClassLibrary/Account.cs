18921925be5a522d5ffedf7d8ccd634efd58484e
tejas1154 <tejas.ramane@aurionpro.com>
2025-07-11 15:20:16 +0530

    Add files via upload

---
+++ MyClassLibrary/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary
+{
+    public class Account
+    {
+        public string AccountNumber { get; set; }
+        public string Name { get; set; }
+        public int Balance { get; set; }
+
+        public Account(string accountNumber, string name, int balance)
+        {
+            AccountNumber = accountNumber;
+            Name = name;
+            Balance = balance;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} AccNo: {AccountNumber} Balance: {Balance}";
+        }
+    }
+}
