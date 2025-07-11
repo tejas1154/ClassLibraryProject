7b71667ce4e79664a031f1deeb6416e9736cbee1
tejas1154 <tejas.ramane@aurionpro.com>
2025-07-11 15:17:24 +0530

    Add files via upload

---
+++ ClassLibraryProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using MyClassLibrary;
+namespace ClassLibraryProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account("1234", "Tejas", 100000);
+            Console.WriteLine(account.ToString());
+        }
+    }
+}
